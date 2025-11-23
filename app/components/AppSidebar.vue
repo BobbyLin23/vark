f4639f6c8d791d436471a764471b52d0d604bb5b
BobbyLin <linzhangsheng23@gmail.com>
2025-11-23 20:14:37 +0800

    feat: add user button

---
+++ app/components/AppSidebar.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import type { SidebarProps } from '@/components/ui/sidebar'
+
+const props = withDefaults(defineProps<SidebarProps>(), {
+  collapsible: 'icon',
+})
+</script>
+
+<template>
+  <Sidebar v-bind="props">
+    <SidebarFooter>
+      <UserButton />
+    </SidebarFooter>
+    <SidebarRail />
+  </Sidebar>
+</template>
--- app/pages/dashboard.vue
+++ app/pages/dashboard.vue
@@ -1,34 +1,28 @@
 <script setup lang="ts">
-import { authClient } from '~/lib/auth-client'
 
-const session = authClient.useSession()
-
-const router = useRouter()
-
-async function handleSignOut() {
-  await authClient.signOut({
-    fetchOptions: {
-      onSuccess: () => {
-        router.push('/auth')
-      },
-    },
-  })
-}
 </script>
 
 <template>
-  <div>
-    <div v-if="session.isPending">
-      <Spinner />
-    </div>
-    <div v-else>
-      <pre>{{ session.data }}</pre>
-      <Button
-        v-if="session.data"
-        @click="handleSignOut()"
-      >
-        Sign out
-      </Button>
-    </div>
-  </div>
+  <SidebarProvider>
+    <AppSidebar />
+    <SidebarInset>
+      <header class="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+        <div class="flex items-center gap-2 px-4">
+          <SidebarTrigger class="-ml-1" />
+          <Separator
+            orientation="vertical"
+            class="mr-2 data-[orientation=vertical]:h-4"
+          />
+        </div>
+      </header>
+      <div class="flex flex-1 flex-col gap-4 p-4 pt-0">
+        <div class="grid auto-rows-min gap-4 md:grid-cols-3">
+          <div class="bg-muted/50 aspect-video rounded-xl" />
+          <div class="bg-muted/50 aspect-video rounded-xl" />
+          <div class="bg-muted/50 aspect-video rounded-xl" />
+        </div>
+        <div class="bg-muted/50 min-h-screen flex-1 rounded-xl md:min-h-min" />
+      </div>
+    </SidebarInset>
+  </SidebarProvider>
 </template>
